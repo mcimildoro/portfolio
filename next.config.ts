de09890ec9a852588ba7481ee2d763a205152a14
mcimildoro <cimildoro@gmail.com>
2025-03-19 18:21:33 +0100

    fix[01]: responsive mode

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-   
     unoptimized: true, // Para evitar optimización de imágenes en local
+  },
+  experimental: {
+    optimizeCss: false,
   },
 };
 
