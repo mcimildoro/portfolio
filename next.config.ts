653ecfe6703e67d15043e86842b09a2678af5c7d
mcimildoro <cimildoro@gmail.com>
2025-03-20 11:01:22 +0100

    fix[04.1]: deploy errors

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     unoptimized: true, // Para evitar optimización de imágenes en local
   },
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false,
   },
 };
 
