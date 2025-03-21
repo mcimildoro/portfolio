e71a9baf7c7ae8c4f0663a15143b5c25932eb1d7
mcimildoro <cimildoro@gmail.com>
2025-03-21 13:29:22 +0100

    feat: agregando google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Urbanist } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { LanguageProvider } from "@/context/LanguageContext";
-
+import Script from "next/script";
 
 const urbanist = Urbanist({subsets: ["latin"]});
 
@@ -19,6 +19,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+       <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-JPP6KN3D36`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JPP6KN3D36', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${urbanist.className} antialiased `} >
           <ThemeProvider
@@ -31,6 +52,7 @@ export default function RootLayout({
             <LanguageProvider>{children}</LanguageProvider>
           </ThemeProvider>
       </body>
+      
     </html>
   );
 }
